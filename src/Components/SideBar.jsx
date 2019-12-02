e0ada9e278c81ff9bee11427320e326b008394d2
luca <l.lobacher@web.de>
2019-12-02 19:01:07 +0100

    Adds semantic ui vertical Menu to SideBar component

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Menu } from 'semantic-ui-react'
 
 const SideBar = () => {
   return (
     <>
-      
+      <Menu text vertical>
+        <Menu.Item header>Luca Lobacher</Menu.Item>
+        <Menu.Item
+          name='About me'
+        />
+        <Menu.Item
+          name='Projects'
+        />
+      </Menu>
     </>
   )
 }
