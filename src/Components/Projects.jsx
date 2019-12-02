ee0481bebcd42c4268766e801e191717447f0143
luca <l.lobacher@web.de>
2019-12-02 21:11:14 +0100

    Adds axios
    Fetches projects data with axios and sets projects state to save data in Projects component.

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Projects extends Component {
+  state = {
+    projects: []
+  }
+
+  componentDidMount() {
+    axios.get('../Data/projects.jsx')
+      .then(response => {
+        this.setState({
+          projects: response.data
+        })
+      })
+  }
+
   render() {
     return (
       <>
