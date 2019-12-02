d1a384b83e1f201ad80c4ddd72ad923e7cc8e955
luca <l.lobacher@web.de>
2019-12-02 20:09:27 +0100

    Adds NavLinks to SideBar
    Adds Route and Switch components from react router and defines the components paths.
    Imports google font family.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import SideBar from './Components/SideBar'
 import HomePage from './Components/HomePage'
 import AboutMe from './Components/AboutMe'
 import Projects from './Components/Projects'
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
 	return (
     <>
       <SideBar />
-      <HomePage />
-      <AboutMe />
-      <Projects />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/about' component={AboutMe} />
+        <Route exact path='/projects' component={Projects} />
+      </Switch>
     </>
   )
 }
--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import { Menu } from 'semantic-ui-react'
+import { NavLink } from 'react-router-dom'
 
 const SideBar = () => {
   return (
     <>
       <Menu text vertical>
-        <Menu.Item header>Luca Lobacher</Menu.Item>
+        <Menu.Item header
+          id='my-name'
+          name='Luca Lobacher'
+          as={NavLink}
+          to='/'
+        />
         <Menu.Item
           name='About me'
+          as={NavLink}
+          to='/about'
         />
         <Menu.Item
           name='Projects'
+          as={NavLink}
+          to='/projects'
         />
       </Menu>
     </>
