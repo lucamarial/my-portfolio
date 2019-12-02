c969ec65636538e4afbb7d1361100ae98a377b7a
luca <l.lobacher@web.de>
2019-12-02 19:07:29 +0100

    Creates AboutMe and Projects components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import SideBar from './Components/SideBar'
+import AboutMe from './Components/AboutMe'
+import Projects from './Components/Projects'
 
 function App() {
 	return (
     <>
       <SideBar />
+      <AboutMe />
+      <Projects />
     </>
   )
 }
