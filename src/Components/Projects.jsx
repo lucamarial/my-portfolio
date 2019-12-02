1822d8d401c2d5a484175fc9d0b13d704d2947c9
luca <l.lobacher@web.de>
2019-12-03 00:47:02 +0100

    Adds Header to Projects component

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import ProjectCard from './ProjectCard'
-import { Container, Grid } from 'semantic-ui-react'
+import { Container, Grid, Header } from 'semantic-ui-react'
 
 class Projects extends Component {
   state = {
@@ -37,7 +37,8 @@ class Projects extends Component {
 
     return (
       <>
-        <Container>
+        <Container className='page-content'>
+          <Header as='h2' textAlign='center'>My Projects</Header>
           <Grid centered container columns={3}>
             <Grid.Row>
               {projectsList}
