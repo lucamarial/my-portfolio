6285570da39406615cc01233771c8905eddb67ad
luca <l.lobacher@web.de>
2019-12-20 21:40:54 +0100

    Adds Grid, Container and Image

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Container, Grid, Header, Image } from 'semantic-ui-react'
+import MyPicture from '../Images/me.jpg'
 
 const AboutMe = () => {
+  let myPicture = <img src={ MyPicture} alt='Image of Luca' /> 
   return (
     <>
-      
+      <Container>
+        <Grid>
+          <Grid.Column width={4}>
+            {myPicture}
+          </Grid.Column>
+          <Grid.Column width={5}>
+            <Header as='h3'>About</Header>
+          </Grid.Column>
+        </Grid>
+      </Container>
     </>
   )
 }
