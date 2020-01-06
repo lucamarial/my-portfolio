6dbb3690db65194068880ee8c796bccbd4a4c417
luca <l.lobacher@web.de>
2020-01-06 20:13:33 +0100

    Changes font color and family

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -15,9 +15,11 @@ const AboutMe = () => {
           <Grid.Column width={8}>
             <h2 id='about-header'>ABOUT</h2>
             <Divider />
-            <p>Yotec nisih tinanit rucila yetih do conule tusonu redil. Mo leri afoten enieb copeg no sesakim feruru.</p>
-            <p> Edietunu unefel tuyehie asinec unienu casiti. Rap vul vaca tanara folu recur keref ileso tigeg. Ho tie titi wan ronu oyifiger na pusep had! Meca tatama elet fap tienap cid ana laramec tu, biel ciemo sal ciret elowados?</p>
-            <p> Nowina dad xohonip lihu yot enonecuc. Nelice setegu sahilal te pep po tisepo voran! Tie ineti opunason detari cates site alelol muyesef ra boyiere? Ser kon ama odisis rer picol citera? Lito omurad sahal; laraya ateb oda okelam. Acetinal sotim lehide kip openaper pin cen di, kor arehiesat cavimel mie. Ronofif len tel arene ramatib fetono ve tahar ibemi ra. Tiererel nilahem ine samo. Gin pire osa luyu hilepat hanonit loru tasutu meha, vused dido do ceb epunebe ritorus tigus onie? Donila nid feno hetoneg asero can tehe zi sahadah redim.</p>
+            <div id='about-text'>
+              <p>Yotec nisih tinanit rucila yetih do conule tusonu redil. Mo leri afoten enieb copeg no sesakim feruru.</p>
+              <p> Edietunu unefel tuyehie asinec unienu casiti. Rap vul vaca tanara folu recur keref ileso tigeg. Ho tie titi wan ronu oyifiger na pusep had! Meca tatama elet fap tienap cid ana laramec tu, biel ciemo sal ciret elowados?</p>
+              <p> Nowina dad xohonip lihu yot enonecuc. Nelice setegu sahilal te pep po tisepo voran! Tie ineti opunason detari cates site alelol muyesef ra boyiere? Ser kon ama odisis rer picol citera? Lito omurad sahal; laraya ateb oda okelam. Acetinal sotim lehide kip openaper pin cen di, kor arehiesat cavimel mie. Ronofif len tel arene ramatib fetono ve tahar ibemi ra. Tiererel nilahem ine samo. Gin pire osa luyu hilepat hanonit loru tasutu meha, vused dido do ceb epunebe ritorus tigus onie? Donila nid feno hetoneg asero can tehe zi sahadah redim.</p>
+            </div>
           </Grid.Column>
         </Grid>
       </Container>
