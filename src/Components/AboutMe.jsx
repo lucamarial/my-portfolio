5262cc437319a256bb3ab032b9d94c9b3159a5e7
luca <l.lobacher@web.de>
2020-01-07 16:23:14 +0100

    Removes Semantic Container and Grid
    Adds image and css grid container.

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,30 +1,27 @@
 import React from 'react'
-import { Container, Grid, Divider } from 'semantic-ui-react'
+import { Divider } from 'semantic-ui-react'
 import MyPicture from '../Images/me.jpg'
+import LucaPicture from '../Images/lucal.jpg'
 
 const AboutMe = () => {
-  let myPicture = <img src={ MyPicture } alt='Luca Lobacher' id='about-img' width='500' height='400' />
+  let myPicture = <img src={ MyPicture } alt='Luca Lobacher' className='about-img' />
+  let lucaPicture = <img src={ LucaPicture } alt='Luca Lobacher' className='about-img' />
 
   return (
     <>
-      <Container id='about-container'>
-        <Grid centered container columns={2}>
-          <Grid.Row>
-            <Grid.Column width={8}>
-              {myPicture}
-            </Grid.Column>
-            <Grid.Column width={8}>
-              <h2 id='about-header'>ABOUT</h2>
-              <Divider section/>
-              <div id='about-text'>
-                <p>Yotec nisih tinanit rucila yetih do conule tusonu redil. Mo leri afoten enieb copeg no sesakim feruru.</p>
-                <p> Edietunu unefel tuyehie asinec unienu casiti. Rap vul vaca tanara folu recur keref ileso tigeg. Ho tie titi wan ronu oyifiger na pusep had! Meca tatama elet fap tienap cid ana laramec tu, biel ciemo sal ciret elowados?</p>
-                <p> Nowina dad xohonip lihu yot enonecuc. Nelice setegu sahilal te pep po tisepo voran! Tie ineti opunason detari cates site alelol muyesef ra boyiere? Ser kon ama odisis rer picol citera? Lito omurad sahal; laraya ateb oda okelam. Acetinal sotim lehide kip openaper pin cen di, kor arehiesat cavimel mie. Ronofif len tel arene ramatib fetono ve tahar ibemi ra. Tiererel nilahem ine samo. Gin pire osa luyu hilepat hanonit loru tasutu meha, vused dido do ceb epunebe ritorus tigus onie? Donila nid feno hetoneg asero can tehe zi sahadah redim.</p>
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
+      <div id='grid-container'>
+        <div id='item-img-one'>{myPicture}</div>
+        <div id='item-img-two'>{lucaPicture}</div>
+        <div id='item-text'>
+          <h2 id='about-header'>ABOUT</h2>
+          <Divider section/>
+          <div id='about-text'>
+            <p id='about-intro'>Yotec nisih tinanit rucila yetih do conule tusonu redil. Mo leri afoten enieb copeg no sesakim feruru.</p>
+            <p> Edietunu unefel tuyehie asinec unienu casiti. Rap vul vaca tanara folu recur keref ileso tigeg. Ho tie titi wan ronu oyifiger na pusep had! Meca tatama elet fap tienap cid ana laramec tu, biel ciemo sal ciret elowados?</p>
+            <p> Nowina dad xohonip lihu yot enonecuc. Nelice setegu sahilal te pep po tisepo voran! Tie ineti opunason detari cates site alelol muyesef ra boyiere? Ser kon ama odisis rer picol citera? Lito omurad sahal; laraya ateb oda okelam. Acetinal sotim lehide kip openaper pin cen di, kor arehiesat cavimel mie. Ronofif len tel arene ramatib fetono ve tahar ibemi ra. Tiererel nilahem ine samo. Gin pire osa luyu hilepat hanonit loru tasutu meha, vused dido do ceb epunebe ritorus tigus onie? Donila nid feno hetoneg asero can tehe zi sahadah redim.</p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
