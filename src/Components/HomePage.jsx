849989cee1c725d838413bc25aee3707e8a00221
luca <l.lobacher@web.de>
2020-01-09 00:51:37 +0100

    Adds image and svg paths to HomePage

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import HomeImg from '../Images/background1.jpg'
 import Bodensee from '../Images/bodensee.jpg'
+import FlowerMeadow from '../Images/flowermeadow.jpg'
 
 const HomePage = () => {
   let homeImg = <img id='home-img' src={HomeImg} alt='Plant' />
   let bodensee = <img id='bodensee-img' src={Bodensee} alt='Luca Lobacher' />
+  let flowerMeadow = <img id='flower-img' src={FlowerMeadow} alt='Flower Meadow' />
+
   return (
     <>
       <div id='home-wrapper'>
@@ -13,10 +16,11 @@ const HomePage = () => {
           <p id='text-one'>Hey, my name is Luca Lobacher.</p>
           <p id='text-two'>I am a front-end developer and this is my portfolio.</p>
         </div>
-        <svg id='draw' width='100%' height='10%' viewBox='0 0 100 10' preserveAspectRatio='none'>
-          <path id='path' d='M0,0 L0,10 100,10 100,0 50,10 Z' />
+        <svg className='draw' width='100%' height='10%' viewBox='0 0 100 10' preserveAspectRatio='none'>
+          <path className='path' d='M0,0 L0,10 100,10 100,0 50,10 Z' />
         </svg>
       </div>
+
       <div id='home-info-grid'>
         <div id='home-item-img'>{bodensee}</div>
         <p id='home-header'>Yotec nisih tinanit</p>
@@ -24,6 +28,16 @@ const HomePage = () => {
           <p>Yotec nisih tinanit rucila yetih do conule tusonu redil. Mo leri afoten enieb copeg no sesakim feruru.</p>
           <p>Yotec nisih tinanit rucila yetih do conule tusonu redil. Mo leri afoten enieb copeg no sesakim feruru.</p>
         </div>
+      </div>
+      
+      <div id='arrow-wrapper'>
+        {flowerMeadow}
+        <svg id='draw-two' width='100%' height='10%' viewBox='0 0 100 10' preserveAspectRatio='none'>
+          <path id='path-two' d='M0,0 L50,10 100,0 Z' />
+        </svg>
+        <svg className='draw' width='100%' height='10%' viewBox='0 0 100 10' preserveAspectRatio='none'>
+          <path className='path' d='M0,0 L0,10 100,10 100,0 50,10 Z' />
+        </svg>
       </div>
     </>
   )
