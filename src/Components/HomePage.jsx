2223ac8165ee85cfc68859583148744ade863106
luca <l.lobacher@web.de>
2020-01-08 19:18:31 +0100

    Adds svg path to Home image and styling

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -12,6 +12,9 @@ const HomePage = () => {
           <p id='text-one'>Hey, my name is Luca Lobacher.</p>
           <p id='text-two'>I am a front-end developer and this is my portfolio.</p>
         </div>
+        <svg id='draw' width='100%' height='10%' viewBox='0 0 100 10' preserveAspectRatio='none'>
+          <path id='path' d='M0,0 L0,10 100,10 100,0 50,10 Z' />
+        </svg>
       </div>
     </>
   )
