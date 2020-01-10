70d960e811887782e8cbffb4e4bd529e07719f07
luca <l.lobacher@web.de>
2020-01-10 15:50:13 +0100

    Creates Contact component and renders it in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import SideBar from './Components/SideBar'
 import HomePage from './Components/HomePage'
 import AboutMe from './Components/AboutMe'
 import Projects from './Components/Projects'
+import Contact from './Components/Contact'
 import { Switch, Route } from 'react-router-dom'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route exact path='/' component={HomePage} />
         <Route exact path='/about' component={AboutMe} />
         <Route exact path='/projects' component={Projects} />
+        <Route exact path='/contact' component={Contact} />
       </Switch>
     </>
   )
