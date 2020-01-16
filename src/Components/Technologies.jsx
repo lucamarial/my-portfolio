0e09b4d3c29ea7f843e5fe406ec8585b7ce86c65
luca <l.lobacher@web.de>
2020-01-16 13:08:30 +0100

    Fetches data from dependencies.json into data state

--- src/Components/Technologies.jsx
+++ src/Components/Technologies.jsx
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Technologies extends Component {
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    axios.get('/data/technologies.json').then(response => {
+      this.setState({
+        data: response.data
+      })
+    })
+  }  
+  
   render() {
     return (
       <>
