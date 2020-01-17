2caa484b1c7d4989b7a75ec53b71a171ec22208b
luca <l.lobacher@web.de>
2020-01-17 13:46:23 +0100

    Refactors code

--- src/Components/Technologies.jsx
+++ src/Components/Technologies.jsx
@@ -2,59 +2,61 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 class Technologies extends Component {
-  state = {
-    data: null
-  }
+	state = {
+		data: null
+	}
 
-  componentDidMount() {
-    axios.get('/data/technologies.json').then(response => {
-      this.setState({
-        data: response.data
-      })
-    })
-  }  
-  
-  render() {
-    const data = this.state.data
-    let frameworkList, skillList, dependencyList
+	componentDidMount() {
+		axios.get('/data/technologies.json').then(response => {
+			this.setState({
+				data: response.data
+			})
+		})
+	}
 
-    if (data) {
-      frameworkList = data.frameworks.map(framework => {
-        return <li>{framework}</li>
-      })
-    }
+	render() {
+		const data = this.state.data
+		let frameworkList, skillList, dependencyList
 
-    if (data) {
-      skillList = data.softSkills.map(skill => {
-        return <li>{skill}</li>
-      })
-    }
+		if (data) {
+			frameworkList = data.frameworks.map(framework => {
+				return <li>{framework}</li>
+			})
+		}
 
-    if (data) {
-      dependencyList = data.dependencies.map(dependency => {
-        return <li>{dependency}</li>
-      })
-    }
+		if (data) {
+			skillList = data.softSkills.map(skill => {
+				return <li>{skill}</li>
+			})
+		}
 
-    return (
-      <>
-        <div id="tech-container">
-          <div id="framework-list">
-            <h1 className="list-header">Frameworks / Libraries</h1>
-            <ul className="comma-list">{frameworkList}</ul>
-          </div>
-          <div id="skill-list">
-            <h1 className="list-header">Soft Skills / Misc</h1>
-            <ul className="comma-list">{skillList}</ul>
-          </div>
-          <div id="dependency-list">
-            <h1 className="list-header">Dependencies etc</h1>
-            <ul className="comma-list">{dependencyList}</ul>
-          </div>
-        </div>      
-      </>
-    )
-  }
+		if (data) {
+			dependencyList = data.dependencies.map(dependency => {
+				return <li>{dependency}</li>
+			})
+		}
+
+		return (
+			<>
+				<div id='tech-container'>
+					<div id='tech-wrapper'>
+						<div id='framework-list'>
+							<p className='list-header'>Frameworks / Libraries</p>
+							<ul className='tech-point'>{frameworkList}</ul>
+						</div>
+						<div id='skill-list'>
+							<p className='list-header'>Soft Skills / Misc</p>
+							<ul className='tech-point'>{skillList}</ul>
+						</div>
+						<div id='dependency-list'>
+							<p className='list-header'>Dependencies etc</p>
+							<ul className='tech-point'>{dependencyList}</ul>
+						</div>
+					</div>
+				</div>
+			</>
+		)
+	}
 }
 
 export default Technologies
