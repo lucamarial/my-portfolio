163ea0af09fbb581068ba06a318c37ac5b6c39dc
luca <l.lobacher@web.de>
2020-01-21 15:03:59 +0100

    Adds semantic ui Grid & Segment with career objective

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import { Grid, Segment } from 'semantic-ui-react'
 
 class Resume extends Component {
   render() {
     return (
       <>
-        
+        <Grid columns='equal'>
+          <Grid.Column>
+            <Segment>
+              <h2>CAREER OBJECTIVE</h2>
+              Quality-oriented recent graduate as a developer from Craft Academy coding Bootcamp seeking new opportunities. I have practical experience working with Ruby on Rails and ReactJS. Looking to learn and grow as a developer.
+            </Segment>
+          </Grid.Column>
+        </Grid>
       </>
     )
   }
