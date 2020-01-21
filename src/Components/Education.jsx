0591eca9523249375f8e14c1279339e70df30ded
luca <l.lobacher@web.de>
2020-01-21 16:48:16 +0100

    Adds List Item from semantic ui
    Returns school, location, degree and date in list item.

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { List } from 'semantic-ui-react'
 
-const Education = () => {
+const Education = (props) => {
+  let item = props.item
+  
   return (
     <>
-      
+      <List.Item key={item.id}>
+        <h2>{item.school}</h2>
+        <p>{item.location} - {item.degree}</p>
+        <p>{item.date}</p>
+      </List.Item>
     </>
   )
 }
