026266917b964a4f114233dc2945951997acc15c
luca <l.lobacher@web.de>
2020-01-21 17:55:25 +0100

    Adds company, location, title and date to List Item

--- src/Components/WorkExperience.jsx
+++ src/Components/WorkExperience.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { List } from 'semantic-ui-react'
 
-export const WorkExperience = () => {
+const WorkExperience = (props) => {
+  let item = props.item
+
   return (
     <>
-      
+      <List.Item key={item.id}>
+        <h3>{item.company}</h3>
+        <p>{item.location} - {item.title}</p>
+        <p>{item.date}</p>
+      </List.Item>
     </>
   )
 }
+
+export default WorkExperience
